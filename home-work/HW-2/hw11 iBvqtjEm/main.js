f88b434b1da87d37eac68762a86df7d31d7cacb4
zhele <demiz53366@gmail.com>
2024-11-08 16:55:58 +0200

    lesson2 hw fix

--- home-work/HW-2/hw11 iBvqtjEm/main.js
+++ home-work/HW-2/hw11 iBvqtjEm/main.js
@@ -2,10 +2,18 @@
 // - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 //     буде присвоювати змінній х значення "default"  якщо ви намагаєтесь присвоїти в неї falsy-значення (хибноподібні, тобто ті, які приводиться до false, а це 0 null undefined і тд).
 
-let x = '';
+// let x = '';
+//
+// if (x === 0 || x === '' || x === undefined || NaN || x === null || x === false) {
+//     console.log('default');
+// } else {
+//     console.log('error');
+// }
 
-if (x === 0 || x === '' || x === undefined || NaN || x === null || x === false) {
-    console.log('default');
-} else {
-    console.log('error');
+
+let x
+
+if (!x) {
+    x = 'default';
 }
+console.log('x');
--- home-work/HW-2/hw3 sA3Gg1sCp/main.js
+++ home-work/HW-2/hw3 sA3Gg1sCp/main.js
@@ -4,20 +4,26 @@ let book1 = {
     title: 'Harry Potter and the Philosopher\'s Stone',
     pageCount: 223,
     genre: 'Fantasy',
-    authors: ['J. K. Rowling', '59-aged']
+    author: [
+        {name: 'J. K. Rowling', age: '59-aged'}
+    ]
 }
 console.log(book1);
 let book2 = {
     title: 'The Lord of the Rings',
     pageCount: 1077,
     genre: 'Fantasy',
-    authors: ['J. R. R. Tolkien', '81-aged']
+    author: [
+        {name: 'J. R. R. Tolkien', age: '81-aged'}
+    ]
 }
 console.log(book2);
 let book3 = {
     title: 'Dune',
     pageCount: '412 (first edition) \, 896 (paperback)',
     genre: 'Science fiction \, Philosophical fiction',
-    authors: ['Frank Herbert', '65-aged']
+    authors: [
+        {name: 'Frank Herbert', age: '65-aged'}
+    ]
 }
 console.log(book3);
