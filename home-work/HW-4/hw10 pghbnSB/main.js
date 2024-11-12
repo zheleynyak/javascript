214dba10c4d2061de57313804dd48f16cff81bf2
zhele <demiz53366@gmail.com>
2024-11-12 12:03:34 +0200

    lesson 3, 4 HW 3, 4

--- home-work/HW-4/hw10 pghbnSB/main.js
+++ home-work/HW-4/hw10 pghbnSB/main.js
@@ -1,2 +1,14 @@
 // #pghbnSB
 // - створити функцію яка повертає найменьше число з масиву
+
+
+let arrNumb = [1, 5, 25, 0, 42, 5, 534, -32, 1, -5434];
+let x = 0;
+
+console.log(arrNumb);
+
+for (let i = 0; i < arrNumb.length; i++) {
+    if (x >= arrNumb[i]) {
+        console.log(arrNumb[i]);
+    }
+}
