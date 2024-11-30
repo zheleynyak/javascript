eb90d0c89942d26097ec366020282711141270b3
zhele <demiz53366@gmail.com>
2024-11-30 06:57:57 +0200

    lesson-7 hw1

--- home-work/HW-7/hw1 XjJuucOMR0/main.js
+++ home-work/HW-7/hw1 XjJuucOMR0/main.js
@@ -2,4 +2,16 @@
 // - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
 // створити пустий масив, наповнити його 10 об'єктами new User(....)
 
-	
+function UserConstructor(id, name, surname, email, phone) {
+	this.id = id;
+	this.name = name;
+	this.surname = surname;
+	this.email = email;
+	this.phone = phone;
+}
+
+let user = [
+	new UserConstructor(1, 'masha', 'lol', 'masha@gmail.com', +38063234324)
+]
+
+console.log(user);
