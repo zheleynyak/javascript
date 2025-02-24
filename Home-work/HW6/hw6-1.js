6e5b4639474e79129329ebad582a19a5ce181e0d
zheleynyak <zheleynyak.denis@gmail.com>
2025-02-24 14:02:34 +0200

    lesson5 and hw4-5

---
+++ Home-work/HW6/hw6-1.js
@@ -0,0 +1,11 @@
+// #dFeorS3m7u
+// - Знайти та вивести довижину настипних стрінгових значень
+//     'hello world', 'lorem ipsum', 'javascript is cool'
+
+
+let str1 = 'hello world';
+console.log(str1.length);
+let str2 = 'lorem ipsum';
+console.log(str2.length);
+let str3 = 'javascript is cool';
+console.log(str3.length);
